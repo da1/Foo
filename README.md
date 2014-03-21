396b3406360b0c22064d79d4103dfaaa26a74d90
da1 <dyamada1988@gmail.com>
2014-03-21 18:41:40 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Foo - It's new $module
+
+# SYNOPSIS
+
+    use Foo;
+
+# DESCRIPTION
+
+Foo is ...
+
+# LICENSE
+
+Copyright (C) da1.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+da1 <dyamada1988@gmail.com>
---
+++ lib/Foo.pm
@@ -0,0 +1,39 @@
+package Foo;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Foo - It's new $module
+
+=head1 SYNOPSIS
+
+    use Foo;
+
+=head1 DESCRIPTION
+
+Foo is ...
+
+=head1 LICENSE
+
+Copyright (C) da1.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+da1 E<lt>dyamada1988@gmail.comE<gt>
+
+=cut
+
